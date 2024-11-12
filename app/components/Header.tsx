a6dd60182f9343dae12f445c63a760011edfa6cb
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2024-11-12 23:55:55 +0900

    headerの修正

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,15 +13,19 @@ const Header: React.FC<HeaderProps> = ({ yearListProps, textFieldProps }) => {
 		<header
 			className={`header flex flex-wrap justify-between items-center w-full shadow-outer-common sticky top-0 z-50 transition-colors duration-300 bg-bg-color`}
 		>
-			<Link to='/' className='header-title text-4xl font-normal ml-12 py-2'>
-				My Photos
-			</Link>
-			<div className='header-right flex mr-24'>
-				<div className='mx-12 my-auto'>
-					<YearList {...yearListProps} />
-				</div>
-				<div className='my-auto'>
-					<TextField {...textFieldProps} />
+			<div className='flex my-2 sm:flex-row w-full justify-between items-center'>
+				<Link to='/' className='header-title text-2xl sm:text-3xl md:text-4xl font-normal ml-4 sm:ml-8 md:ml-12 py-2'>
+					My Photos
+				</Link>
+				<div className='flex flex-row items-center'>
+					<div className='mx-4 sm:mx-8 md:mx-12 my-auto hidden sm:block'>
+						<TextField {...textFieldProps} />
+
+						<TextField {...textFieldProps} />
+					</div>
+					<div className='mx-4 sm:mx-8 md:mx-12 my-auto'>
+						<YearList {...yearListProps} />
+					</div>
 				</div>
 			</div>
 		</header>
